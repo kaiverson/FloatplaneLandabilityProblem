8bd96b2619ba552f2373d154c0d5e5fda3f14dbd
Pat Pragman <pat@pragman.io>
2024-04-19 12:14:51 -0800

    restructuring and cleaning

--- main.py
+++ main.py
@@ -6,12 +6,18 @@
 
 
 import os
+from copy import deepcopy
 import pandas as pd
-from functions.files import *
-from functions.polygons import *
-from functions.visualization import map_lakes
-from functions.statistics import generate_positive_identification_statistics
-from functions.debugging_tools import stop_watch
+import numpy as np
+from toolbox.files import read_polygons_from_csv, export_polygons_from_raw_vertices
+from toolbox.polygons import (has_length_within_polygon_naive, average_vertice_location,
+                              find_most_common_id_and_remove, is_point_in_polygon,
+                              edge_lengths_of_polygon, lat_lon_to_meters, raw_vertices_to_df)
+
+from toolbox.visualization import map_lakes
+from toolbox.statistics import generate_positive_identification_statistics
+from toolbox.debugging_tools import stop_watch
+from toolbox.constants import MATSU_REGION_OF_INTEREST as roi
 
 
 
@@ -40,8 +46,7 @@ def main_function(target_meters=500.0,
     return:
         the successful polygons in a dataframe
     """
-    # TODO: make code more readable.
-    # TODO: make output more readable.
+    # TODO: make code more readable.  Halfway done!
     # TODO: add more polygon information. Min edge, max edge, etc.
     abs_polygons_path = os.path.abspath(polygons_path)
     if not os.path.exists(abs_polygons_path):
@@ -104,6 +109,25 @@ def main_function(target_meters=500.0,
     return find_most_common_id_and_remove(raw_vertices_to_df(passing_polygons))
 
 
+def filter_source_of_truth(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Takes the dataframe containing the source of truth, then filters out the stuff that is not relevant
+    Args:
+        df: source of truth
+
+    Returns: a filtered source of truth that doesn't contain lakes outside of the ROI or non-floatplane airfields
+    and airports
+    """
+
+    working_df = deepcopy(df[df['floatplanes'] == 1])  # we don't care about runways, get rid of them
+
+    """
+    we need to filter out places that aren't in our ROI
+    """
+    working_df['roi'] = working_df.apply(lambda r: is_point_in_polygon(roi, (r["Lat"], r['Long'])), axis='columns')
+    return working_df[working_df['roi']]
+
+
 if __name__ == "__main__":
     """
     The main function here should be where we fit together all the pieces of our model.  This is called the "pipe and
@@ -112,13 +136,9 @@ if __name__ == "__main__":
     I went ahead and refactored everything so that the model produces a list of dataframes of successful polygons, then
     concats those together, visualizes them, and checks to see how many points we got right with our algorithm.
     """
-    # TODO:  Refactor some of this code to be inside abstracted functions
     # TODO:  Create Diagram for Pipe-Filter Architectural Flow of Code
 
-
-    csv_list = ['lakes_csv/lake_boundaries_1.csv', 'lakes_csv/lake_boundaries_2.csv', 'lakes_csv/lake_boundaries_3.csv',
-                'lakes_csv/lake_boundaries_4.csv']
-
+    csv_list = [os.path.join("lakes_csv", filename) for filename in os.listdir("lakes_csv") if filename.endswith(".csv")]
     successful_polygons = [main_function(polygons_path=csv_file) for csv_file in csv_list]
 
     # make all the polygon indices unique by adding the length of the previous df to the polygon
@@ -130,35 +150,8 @@ if __name__ == "__main__":
     # put all of these together and calculate the min and max lat-lons
     df = pd.concat(successful_polygons)
 
-    # now load the source of truth,
-    lake_truth = pd.read_csv('flight_data/cleaned.csv')
-    lake_truth = lake_truth[lake_truth['floatplanes'] == 1]  # we don't care about runways
-
-    """
-    we need to filter out places that aren't in our ROI
-    """
-    matSuROI = [
-        (-151.6970688287938, 60.90480020426519),
-        (-151.3489595751618, 61.00629812504111),
-        (-151.1459775803294, 61.04351337219632),
-        (-150.9369163348004, 61.19936156775925),
-        (-150.6092949437964, 61.27478397462557),
-        (-150.4866081744711, 61.24217854425234),
-        (-149.9790927356937, 61.23316377103141),
-        (-149.8644558219648, 61.35567989288409),
-        (-149.7988576878899, 61.39628094061301),
-        (-149.6061035585755, 61.49071772290527),
-        (-149.2577295764331, 61.48164025046362),
-        (-148.9007510082246, 63.38768837583029),
-        (-151.0072124599679, 63.06933025031949),
-        (-153.8934978076406, 62.50953235888112),
-        (-151.6970688287938, 60.90480020426519)
-    ]
-    matSuROI = [(lat, lon) for lon, lat in matSuROI]  # reverse this because gee expects it the other way
-    lake_truth['roi'] = lake_truth.apply(lambda r: is_point_in_polygon(matSuROI, (r["Lat"], r['Long'])), axis='columns')
-    lake_truth = lake_truth[lake_truth['roi']]
-
-
+    # now load the source of truth and filter out unnecessary information
+    lake_truth = filter_source_of_truth(pd.read_csv('flight_data/cleaned.csv'))
 
     # now we need to check each point in the source of truth data and see if it's bounded by one of the polygons
     lake_truth = generate_positive_identification_statistics(df, lake_truth, verbose=True)
